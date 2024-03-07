625bfc4d46fb858d8768d246bf615016044aadaa
Mihail Verejan <35393546+future-apocalypse@users.noreply.github.com>
2024-03-07 16:08:47 +0200

    Project adjustments

---
+++ My project/Assets/_Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
